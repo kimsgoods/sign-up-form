d3cc78ce5c133114643405b613854009735247a8
Kim <kimescabarte@gmail.com>
2022-08-18 11:38:36 +0800

    Update code

---
+++ script.js
@@ -0,0 +1,13 @@
+let password = document.getElementById("password")
+let confirmPassword = document.getElementById("confirm");
+
+function validatePassword(){
+  if(password.value != confirmPassword.value) {
+    confirmPassword.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirmPassword.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirmPassword.onkeyup = validatePassword;
